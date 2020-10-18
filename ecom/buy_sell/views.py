6aa6f4e0a240734d8ad373283e8d29decb4bed56
ramirov94 <64160242+ramirov94@users.noreply.github.com>
2020-10-18 14:16:54 -0700

    Added H2 to index.html

--- ecom/buy_sell/views.py
+++ ecom/buy_sell/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render, redirect, HttpResponse
+# from django.shortcuts import render, redirect, HttpResponse
+# from django.views.generic import TemplateView
 
-def index(request):
-    return render(request, 'index.html')
+
+# def index(request):
+#     return render(request, "frontend/build/index.html")
+    # return render(request, 'index.html')
+
+
+
+# catchall = TemplateView.as_view(template_name='index.html')
+
 
 
--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('buy_sell.urls')),
+    path("", TemplateView.as_view(template_name= "index.html")),
+    # path('', include('buy_sell.urls')), // Commented out to not interfere with above path
 ]
